52c1bfa1feccab9619f13df9307a8453bc04ece7
xavier <xg@siat.fr>
2017-01-12 11:55:38 +0100

    Seconde etape

--- monProjet/monProjet/src/monProjet/MaClasse.java
+++ monProjet/monProjet/src/monProjet/MaClasse.java
@@ -5,8 +5,7 @@ public class MaClasse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
-		System.out.println("Evolution du projet");
-		
+		System.out.println("Evolution du projet");		
 	}
 
 }

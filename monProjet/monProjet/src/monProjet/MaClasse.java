8a5c012de76608f180619048f4bbecd8f597162b
xavier <xg@siat.fr>
2017-01-12 12:58:35 +0100

    Pour l'instant, commit partiel

--- monProjet/monProjet/src/monProjet/MaClasse.java
+++ monProjet/monProjet/src/monProjet/MaClasse.java
@@ -5,7 +5,8 @@ public class MaClasse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
-		System.out.println("Evolution du projet");		
+		System.out.println("Evolution du projet");
+		System.out.println("Troisime version déja");
 	}
 
 }
